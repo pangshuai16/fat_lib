7e0048f72be7f574c5acc04f2bef42be5189bdaf
pangshuai16 <chauncytu@qq.com>
2022-11-04 19:16:17 +0800

    feat: 去除math的$前缀

--- src/math.ts
+++ src/math.ts
@@ -1,7 +1,7 @@
 /**
  * 计算数组的总和
  */
-export function $sum(data: number[]) {
+export function sum(data: number[]) {
   return data.reduce((sum, now) => {
     return sum + now
   })
@@ -10,15 +10,15 @@ export function $sum(data: number[]) {
 /**
  * 计算数组的平均数
  */
-export function $avg(data: number[]) {
-  return $sum(data) / data.length
+export function avg(data: number[]) {
+  return sum(data) / data.length
 }
 
 /**
  * 计算数组的方差
  */
-export function $variance(data: number[]) {
-  const avgValue = $avg(data)
+export function variance(data: number[]) {
+  const avgValue = avg(data)
   return data.reduce((pre, now) => {
     return Math.pow(now - avgValue, 2) + pre
   }) / data.length
@@ -27,6 +27,6 @@ export function $variance(data: number[]) {
 /**
  * 计算数组的标准差
  */
-export function $std(data: number[]) {
-  return Math.sqrt($variance(data))
+export function std(data: number[]) {
+  return Math.sqrt(variance(data))
 }
