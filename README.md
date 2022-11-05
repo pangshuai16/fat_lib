0356bfca1894cbdf835fb013ce37ea58d79725f7
pangshuai16 <chauncytu@qq.com>
2022-11-05 23:00:42 +0800

    fix: 修复左连接与内连接

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # fat_lib
 
-自用的工具库
+## Data
+
+### leftJoin
+
+这个函数能根据`keys`左连接两个数组，并根据`params`返回一个新数组。
+
+This function merges two arrays according to `keys` and returns a new array based on `params`.
+
+- input
+    - leftArray: object[],
+    - rightArray: object[],
+    - keys: string | string[],
+    - params?: string | string[],
+    - fillValue?: string | number | "preValue" | undefined | null
+- output
+    - object[]
+
+### innerJoin
+
+这个函数能根据`keys`内连接两个数组，并根据`params`返回一个新数组。
+
+This function merges two arrays according to `keys` and returns a new array based on `params`.
+
+- input
+    - leftArray: object[],
+    - rightArray: object[],
+    - keys: string | string[],
+    - params?: string | string[],
+    - fillValue?: string | number | "preValue" | undefined | null
+- output
+    - object[]
--- src/array.ts
+++ src/array.ts
@@ -1,71 +1,109 @@
 /**
  * @desc 给定两个数组左连接
-*/
+ */
+import {type} from "./type";
 
-export function leftJoin(config: { leftArray: object[], rightArray: object[], keys: string | string[], params: string | string[], fillValue?: string | number | null | undefined | 'preValue' }) {
-  let { leftArray, rightArray, keys, params, fillValue } = config
-  // 初始化keys,params
-  keys = typeof keys === 'string' ? [keys] : keys
-  params = (typeof params === 'string' ? [params] : params)
-  const fillValueB = fillValue === 'preValue' ? undefined : fillValue
-  const data = leftArray.map((left: any) => {
-    // 获取对应的object
-    const right = rightArray.find((right: any) => (keys as any[]).every(key => left[key] === right[key]))
+export function leftJoin(config: {
+    leftArray: Object[],
+    rightArray: Object[],
+    keys: string | string[],
+    filed?: string | string[],
+    fillValue?: string | number | null | undefined | 'preValue'
+}) {
+    let {leftArray, rightArray, keys, filed, fillValue} = config
+    // 初始化参数
+    const keyList: string[] = typeof keys === 'string' ? [keys] : keys
+    const leftFiled = getAllKeys(leftArray)
+    const rightFiled = getAllKeys(rightArray)
+    switch (type(filed)) {
+        case "string":
+            filed = [filed as string]
+            break
+        case "array":
+            filed = filed as string[]
+            break
+        default:
+            filed = [...new Set([...leftFiled, ...rightFiled])]
+    }
+    const outData: any[] = []
+    leftArray.forEach((left: any, leftIndex) => {
+        // 获取对应的right
+        let right: any = rightArray.find((right: any) => {
+            return keyList.every(key => (left[key]) === right[key])
+        })
+        if (!right) {
+            right = {}
+        }
 
-    // 替换对应值
-    //@ts-ignore
-    params.forEach(param => {
-      // @ts-ignore
-      left[param] = right?.[param] ?? fillValueB
+        // 左连接
+        const item: any = {};
+        (filed as string[]).forEach((key: string) => {
+            if (leftFiled.includes(key)) {
+                item[key] = left[key]
+            } else if (fillValue === 'preValue') {
+                const preIndex = leftIndex > 0 ? leftIndex - 1 : 0
+                item[key] = right[key] ?? outData[preIndex]?.[key]
+            } else {
+                item[key] = right[key] ?? fillValue
+            }
+        })
+        outData.push(item)
     })
-    return left
-  })
-
-  // 按preValue填充空值
-  if (fillValue === 'preValue') {
-    data.forEach((row, index) => {
-      //@ts-ignore
-      params.forEach(param => {
-        // @ts-ignore
-        row[param] = row[param] ?? data[index - 1][param] ?? undefined
-      })
-    })
-  }
-  return data
+    return outData
 }
 
 /**
  * @desc 两数组内连接
  */
-export function innerJoin(config: { leftArray: object[], rightArray: object[], keys: string | string[], params: string | string[], fillValue?: any }) {
-  let { leftArray, rightArray, keys, params, fillValue } = config
-  // 初始化keys,params
-  keys = typeof keys === 'string' ? [keys] : keys
-  params = (typeof params === 'string' ? [params] : params)
-  const fillValueB = fillValue === 'preValue' ? undefined : fillValue
-  const data: object[] = []
-  leftArray.forEach((left: any) => {
-    const right = rightArray.find((right: any) => {
-      return (keys as string[]).every(key => left[key] === right[key])
-    })
-    if (!right) return
-    // @ts-ignore
-    params.forEach(param => {
-      // @ts-ignore
-      left[param] = right?.[param] ?? fillValueB
-    })
-    data.push(left)
-  })
+export function innerJoin(config: {
+    leftArray: Object[],
+    rightArray: Object[],
+    keys: string | string[],
+    filed?: string | string[],
+    fillValue?: string | number | null | undefined | 'preValue'
+}) {
+    let {leftArray, rightArray, keys, filed, fillValue} = config
+    // 初始化参数
+    const keyList: string[] = typeof keys === 'string' ? [keys] : keys
+    const leftFiled = getAllKeys(leftArray)
+    const rightFiled = getAllKeys(rightArray)
+    switch (type(filed)) {
+        case "string":
+            filed = [filed as string]
+            break
+        case "array":
+            filed = filed as string[]
+            break
+        default:
+            filed = [...new Set([...leftFiled, ...rightFiled])]
+    }
+    const outData: any[] = []
 
-  // 按preValue填充空值
-  if (fillValue === 'preValue') {
-    data.forEach((row, index) => {
-      //@ts-ignore
-      params.forEach(param => {
-        // @ts-ignore
-        row[param] = row[param] ?? data[index - 1][param] ?? undefined
-      })
+    leftArray.forEach((left: any) => {
+        const right:any = rightArray.find((right: any) => {
+            return (keyList as string[]).every(key => left[key] === right[key])
+        })
+        if (!right) return
+        const item:any = {};
+        (filed as string[]).forEach((key: string) => {
+            if (leftFiled.includes(key)) {
+                item[key] = left[key]
+                console.log(key,item[key],outData)
+            } else if (fillValue === 'preValue') {
+                const preIndex = outData.length > 1 ? outData.length - 2 : 0
+                console.log(preIndex,outData[preIndex]?.[key],key)
+                item[key] = right[key] ?? outData[preIndex]?.[key]
+            } else {
+                console.log('else',key)
+                item[key] = right[key] ?? fillValue
+            }
+        })
+        outData.push(item)
     })
-  }
-  return data
+    return outData
+}
+
+export function getAllKeys(array: Object[]) {
+    const keys: string[] = array.map(item => Object.keys(item)).flat()
+    return [...new Set(keys)]
 }
